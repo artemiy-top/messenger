4d5ebacffaca99cc18a785bc3677aa2135b92518
Artemiy <arhimondus@vk.com>
2026-05-29 18:12:15 +0300

    feat: ClientChat

---
+++ Shared/ClientChat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class ClientChat
+    {
+        public required string Title { get; set; }
+        public required int Id { get; set; }
+        public required string? LastMessage { get; set; }
+    }
+}
